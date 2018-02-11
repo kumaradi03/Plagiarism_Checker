4f5758f4e92a66c35cf8ce2324edcae03a3f1220
Prathamesh Tajane <ptajane@gmail.com>
2018-02-11 16:19:56 -0500

    final commit for Phase B deliverables

--- phaseB/src/interfaces/ASTClass.java
+++ phaseB/src/interfaces/ASTClass.java
@@ -1,7 +1,12 @@
 package interfaces;
+import java.util.List;
 
 /**
  * This is an abstract class representing AST of specific code file tokens
  */
 public abstract class ASTClass {
+    /**
+     * This ASTNodes list will store ASt for a file
+     */
+    List<String> ASTList;
 }
