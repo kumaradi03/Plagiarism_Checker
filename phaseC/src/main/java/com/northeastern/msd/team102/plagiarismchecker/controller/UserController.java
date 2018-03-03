561d2ef510c3fa3dfdbcd98ddfa73ff911d79653
Prathamesh Tajane <ptajane@gmail.com>
2018-03-03 01:36:19 -0500

    resolved merge conflicts

--- phaseC/src/main/java/com/northeastern/msd/team102/plagiarismchecker/controller/UserController.java
+++ phaseC/src/main/java/com/northeastern/msd/team102/plagiarismchecker/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.northeastern.msd.team102.plagiarismchecker.controller;
 
-
-
+import com.northeastern.msd.team102.plagiarismchecker.constants.URI;
 import com.northeastern.msd.team102.plagiarismchecker.entity.User;
 import com.northeastern.msd.team102.plagiarismchecker.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +8,14 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/rest/user")
+@RequestMapping(value = URI.USERS_BASE_URI)
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping("/login")
-    public User Login(@RequestBody User user) {
+    @PostMapping(value = URI.USERS_LOGIN_URI)
+    public User Login(@RequestBody User user){
         return userService.findUserByCredentials(user);
     }
 }
