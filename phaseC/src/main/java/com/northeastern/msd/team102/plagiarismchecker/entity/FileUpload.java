44eb035523b2552572f3de6f776abb7637f301f9
kumaradi <kumaradi@ccs.neu.edu>
2018-03-20 15:16:07 -0400

    CS102-31 #comment Adding Javadocs for FileUpdate

--- phaseC/src/main/java/com/northeastern/msd/team102/plagiarismchecker/entity/FileUpload.java
+++ phaseC/src/main/java/com/northeastern/msd/team102/plagiarismchecker/entity/FileUpload.java
@@ -2,6 +2,9 @@ package com.northeastern.msd.team102.plagiarismchecker.entity;
 
 import javax.persistence.*;
 
+/**
+ * FileUpload entity.
+ */
 @Entity
 public class FileUpload {
 
@@ -25,61 +28,115 @@ public class FileUpload {
     @JoinColumn(foreignKey = @ForeignKey(name = "user_homework_association"))
     private User user;
 
-
+    /**
+     * Constructor for FileUpload class
+     * @param filename File name
+     * @param file file in bytes array
+     * @param mimeType mimeType of the file.
+     */
     public FileUpload (String filename, byte[] file, String mimeType) {
         this.file = file;
         this.filename = filename;
         this.mimeType = mimeType;
     }
 
+    /**
+     *  Default Constructor
+     */
     public FileUpload() {
-        // Default Constructor
+
     }
 
+    /**
+     * Getter for Homework.
+     * @return the homework object for the given file.
+     */
     public Homework getHomework() {
         return homework;
     }
 
+    /**
+     * Setter for homework
+     * @param homework Homework object for the given file.
+     */
     public void setHomework(Homework homework) {
         this.homework = homework;
     }
 
+    /**
+     * Getter for User.
+     * @return returns the user for the particular file.
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Setter for User.
+     */
     public void setUser(User user) {
         this.user = user;
     }
 
+    /**
+     * Getter for id.
+     * @return returns the id for the file.
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Setter for id.
+     */
     public void setId(long id) {
         this.id = id;
     }
 
+    /**
+     * Getter for filename.
+     * @return returns the name of the file.
+     */
     public String getFilename() {
         return filename;
     }
 
+    /**
+     * Setter for filename.
+     * @param filename name of the file.
+     */
     public void setFilename(String filename) {
         this.filename = filename;
     }
 
+    /**
+     * Getter for file in byte array .
+     * @return returns the file in byte array.
+     */
     public byte[] getFile() {
         return file;
     }
 
+    /**
+     * Setter for file.
+     * @param file byte array of the file.
+     */
     public void setFile(byte[] file) {
         this.file = file;
     }
 
+    /**
+     * Getter for mimeType .
+     * @return returns the mimeType of the file.
+     */
     public String getMimeType() {
         return mimeType;
     }
 
+    /**
+     * Setter for mimeType
+     * @param mimeType mimeType of the file
+     */
     public void setMimeType(String mimeType) {
         this.mimeType = mimeType;
     }
