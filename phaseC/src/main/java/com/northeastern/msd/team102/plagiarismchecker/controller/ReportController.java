c2ec21cba6f460d1bd649a6e4711b8582c7dab45
kumaradi <kumaradi@ccs.neu.edu>
2018-03-20 14:54:15 -0400

    CS102-31 #comment Adding Javadoc for ReportController

--- phaseC/src/main/java/com/northeastern/msd/team102/plagiarismchecker/controller/ReportController.java
+++ phaseC/src/main/java/com/northeastern/msd/team102/plagiarismchecker/controller/ReportController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Controller class for User
+ */
 @RestController
 @RequestMapping("/rest/report")
 public class ReportController {
@@ -14,6 +17,13 @@ public class ReportController {
     @Autowired
     private ReportService reportService;
 
+    /**
+     * findAllReportSummary method which receives the request for fetching the plagiarism reports foa given user
+     * and homework.
+     * @param userId User Id
+     * @param hwId Homework Id
+     * @return returns all the reports for for this user and homework
+     */
     @GetMapping("/findAllReportSummary")
     public List<Report> findAllReportSummary(@RequestParam long userId, @RequestParam long hwId) {
         return reportService.findAllReportSummary(userId, hwId);
