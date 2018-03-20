e2442eeb12efe411d5564e816e17e0efb4a77e33
kumaradi <kumaradi@ccs.neu.edu>
2018-03-20 15:31:02 -0400

    CS102-31 #comment Adding Javadocs for Report

--- phaseC/src/main/java/com/northeastern/msd/team102/plagiarismchecker/entity/Report.java
+++ phaseC/src/main/java/com/northeastern/msd/team102/plagiarismchecker/entity/Report.java
@@ -1,6 +1,9 @@
 package com.northeastern.msd.team102.plagiarismchecker.entity;
 import javax.persistence.*;
 
+/**
+ * Report entity.
+ */
 @Entity
 public class Report {
     @Id
@@ -31,6 +34,15 @@ public class Report {
     @Column(name = "percentage")
     private double percentage;
 
+    /**
+     * Constructor for Report class
+     * @param user1 User whose report is to be generated
+     * @param user2 User against which  the report is created
+     * @param fileUpload1 file for User1
+     * @param fileUpload2 file for User1
+     * @param homework homework for which report is generated
+     * @param percentage percentage plagiarism for the two files
+     */
     public Report(User user1, User user2, FileUpload fileUpload1, FileUpload fileUpload2, Homework homework, double percentage) {
         this.user1 = user1;
         this.user2 = user2;
@@ -40,62 +52,121 @@ public class Report {
         this.percentage = percentage;
     }
 
+    /**
+     * Default constructor
+     */
     public Report() {
 
     }
 
+    /**
+     * Getter for reportId.
+     * @return returns the Id of the generated report.
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Setter for reportId.
+     * @param id
+     */
     public void setId(long id) {
         this.id = id;
     }
 
+    /**
+     * Getter for User whose report is to be generated.
+     * @return User object
+     */
     public User getUser1() {
         return user1;
     }
 
+    /**
+     * Setter for User whose report is to be generated.
+     * @param user1
+     */
     public void setUser1(User user1) {
         this.user1 = user1;
     }
 
+    /**
+     * Getter for User against which  the report is created.
+     * @return User object
+     */
     public User getUser2() {
         return user2;
     }
 
+    /**
+     * Setter for User against which  the report is created.
+     * @param user2
+     */
     public void setUser2(User user2) {
         this.user2 = user2;
     }
 
+    /**
+     * Getter for plagiarism percentage for the two files.
+     * @return double percentage.
+     */
     public double getPercentage() {
         return percentage;
     }
 
+    /**
+     * Setter for plagiarism percentage for the two files.
+     * @param percentage
+     */
     public void setPercentage(double percentage) {
         this.percentage = percentage;
     }
 
+    /**
+     * Getter for file for User1.
+     * @return file for user1.
+     */
     public FileUpload getFileUpload1() {
         return fileUpload1;
     }
 
+    /**
+     * Setter for file for User1.
+     * @param fileUpload1
+     */
     public void setFileUpload1(FileUpload fileUpload1) {
         this.fileUpload1 = fileUpload1;
     }
 
+    /**
+     * Getter for file for User2.
+     * @return file for user2.
+     */
     public FileUpload getFileUpload2() {
         return fileUpload2;
     }
 
+    /**
+     * Setter for file for User2.
+     * @param fileUpload2
+     */
     public void setFileUpload2(FileUpload fileUpload2) {
         this.fileUpload2 = fileUpload2;
     }
 
+    /**
+     * Getter for homework for which report is generated.
+     * @return homework for which the particular report is generated.
+     */
     public Homework getHomework() {
         return homework;
     }
 
+    /**
+     * Setter for homework for which report is generated.
+     * @param homework
+     */
     public void setHomework(Homework homework) {
         this.homework = homework;
     }
