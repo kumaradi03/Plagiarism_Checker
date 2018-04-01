cc87472c6b5c7c81f4759705991f7653a877e855
mrunal <mrunalghanwat2195@gmail.com>
2018-04-01 16:34:41 -0400

    LCS.java updated to fetch lines of python files

--- phaseC/src/main/java/com/northeastern/msd/team102/plagiarismchecker/antlr/ast/LCS.java
+++ phaseC/src/main/java/com/northeastern/msd/team102/plagiarismchecker/antlr/ast/LCS.java
@@ -1,7 +1,6 @@
 package com.northeastern.msd.team102.plagiarismchecker.antlr.ast;
 
 /**
- * @author mrunal
  * The class is responsible to generate
  * similar chunk of code among two different python programs.
  * reference for Longest Common subsequence : https://www.geeksforgeeks.org/print-longest-common-substring/
@@ -9,63 +8,73 @@ package com.northeastern.msd.team102.plagiarismchecker.antlr.ast;
  */
 public class LCS {
 	
-	static String LCSSubString(String X, String Y)
+	/**
+	 * 
+	 * @param original : String
+	 * @param suspected : String
+	 * @return substring String - Longest common substring of original and suspected string.
+	 */
+	static String LcsSubString(String original, String suspected)
     {
-        /** table to store lengths of longest common
-         suffixes of substrings.   Note that LCSuff[i][j]
-         contains length of longest common suffix of X[0..i-1]
-         and Y[0..j-1]*/
-		int m = X.length();
-		int n= Y.length();
-        int[][] LCSuff = new int[ m+ 1][n + 1];
+		/**
+		 * Table to store lengths of longest common suffixes of substrings. Note that
+		 * LCSuff[i][j] contains length of longest common suffix of X[0..i-1] and
+		 * Y[0..j-1]
+		 */
+		int originalLength = original.length();
+		int suspectedLength= suspected.length();
+        int[][] lcsSuffix = new int[ originalLength+ 1][suspectedLength + 1];
  
-        // To store length of the longest common substring
-        int len = 0;
+        /**
+         * To store length of the longest common substring
+         */
+        int commonLength = 0;
  
-        // To store the index of the cell which contains the 
-        // maximum value. This cell's index helps in building 
-        // up the longest common substring from right to left.
-        int row = 0, col = 0;
- 
-        /* Following steps build LCSuff[m+1][n+1] in bottom
-           up fashion. */
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
+        /** To store the index of the cell which contains the 
+         * maximum value. This cell's index helps in building 
+         * up the longest common substring from right to left.
+         */
+         int row = 0, column = 0;
+        
+         /**
+          * Longest Common Substring.
+          */
+         String lcSubstring  = "";
+        
+        for (int i = 0; i <= originalLength; i++) {
+            for (int j = 0; j <= suspectedLength; j++) {
                 if (i == 0 || j == 0)
-                    LCSuff[i][j] = 0;
+                    lcsSuffix[i][j] = 0;
  
-                else if (X.charAt(i - 1) == Y.charAt(j - 1)) {
-                    LCSuff[i][j] = LCSuff[i - 1][j - 1] + 1;
-                    if (len < LCSuff[i][j]) {
-                        len = LCSuff[i][j];
+                else if (original.charAt(i - 1) == suspected.charAt(j - 1)) {
+                    lcsSuffix[i][j] = lcsSuffix[i - 1][j - 1] + 1;
+                    if (commonLength < lcsSuffix[i][j]) {
+                        commonLength = lcsSuffix[i][j];
                         row = i;
-                        col = j;
+                        column = j;
                     }
                 } else
-                    LCSuff[i][j] = 0;
+                    lcsSuffix[i][j] = 0;
             }
         }
  
-        // if true, then no common substring exists
-        if (len == 0) {
-            System.out.println("No Common Substring");
+        /**
+         * if true, then no common substring exists
+         */
+        if (commonLength == 0) {
+            return lcSubstring;
         }
  
-        // allocate space for the longest common substring
-        String resultStr = "";
- 
-        // traverse up diagonally form the (row, col) cell
-        // until LCSuff[row][col] != 0
-        while (LCSuff[row][col] != 0) {
-            resultStr  = X.charAt(row - 1) + resultStr; // or Y[col-1]
-            --len;
-             
-            // move diagonally up to previous cell
+        /**
+         *  traverse up diagonally form the (row, col) cell
+         *  until lcsSuffix[row][col] != 0
+         */
+        while (lcsSuffix[row][column] != 0) {
+        	lcSubstring  = original.charAt(row - 1) + lcSubstring; // or Y[col-1]
+            --commonLength;
             row--;
-            col--;
+            column--;
         }
- 
-        // required longest common substring 
-        return resultStr;
+        return lcSubstring;
     }
 }
