c277e67b5dc53fa16109fd7bc6aed024243b6002
Prathamesh Tajane <ptajane@gmail.com>
2018-04-15 16:16:25 -0400

    added test cases and javadoc

--- phaseC/src/test/java/com/northeastern/msd/team102/plagiarismchecker/controller/HomeworkControllerTest.java
+++ phaseC/src/test/java/com/northeastern/msd/team102/plagiarismchecker/controller/HomeworkControllerTest.java
@@ -35,25 +35,34 @@ public class HomeworkControllerTest {
     @MockBean
     private HomeworkService homeworkService;
 
-
+    /**
+     * This test case verifies functionality of creating new Homework for given userId
+     *
+     * @throws Exception
+     */
     @Test
     public void createHomework() throws Exception {
         Homework testHomeWork = new Homework();
         testHomeWork.setName("HomeWork1");
         testHomeWork.setDescription("Home Work1 description");
-        String testJson="{\"id\":0,\"name\":\"HomeWork1\",\"description\":\"Home Work1 description\",\"user\":null}";
-        String ExpectedOutput="";
-        Mockito.when(homeworkService.createHomework(testHomeWork,3,2)).thenReturn(testHomeWork);
+        String testJson = "{\"id\":0,\"name\":\"HomeWork1\",\"description\":\"Home Work1 description\",\"user\":null}";
+        String ExpectedOutput = "";
+        Mockito.when(homeworkService.createHomework(testHomeWork, 3, 2)).thenReturn(testHomeWork);
         RequestBuilder requestBuilder = MockMvcRequestBuilders.post(
                 "/rest/homework/create").accept(MediaType.APPLICATION_JSON).content(testJson)
                 .contentType(MediaType.APPLICATION_JSON)
-                .param("userId","3")
-                .param("courseId","2");
+                .param("userId", "3")
+                .param("courseId", "2");
         MvcResult result = mockMvc.perform(requestBuilder).andReturn();
         assertEquals(ExpectedOutput, result.getResponse()
                 .getContentAsString());
     }
 
+    /**
+     * This test case verifies functionality of finding all homeworks associated to given Course Id
+     *
+     * @throws Exception
+     */
     @Test
     public void findAllHomeworkForCourse() throws Exception {
         Homework testHomeWork = new Homework();
@@ -61,10 +70,10 @@ public class HomeworkControllerTest {
         testHomeWork.setDescription("Home Work1 description");
         List<Homework> testHomeWorkList = new ArrayList<>();
         testHomeWorkList.add(testHomeWork);
-        String ExpectedOutput="[{\"id\":0,\"name\":\"HomeWork1\",\"description\":\"Home Work1 description\",\"user\":null,\"course\":null}]";
+        String ExpectedOutput = "[{\"id\":0,\"name\":\"HomeWork1\",\"description\":\"Home Work1 description\",\"user\":null,\"course\":null}]";
         Mockito.when(homeworkService.findAllByCourseId(3)).thenReturn(testHomeWorkList);
         MvcResult result;
-        result=mockMvc.perform(MockMvcRequestBuilders.get("/rest/homework/findAllHomeworkForCourse").param("courseId","3"))
+        result = mockMvc.perform(MockMvcRequestBuilders.get("/rest/homework/findAllHomeworkForCourse").param("courseId", "3"))
                 .andExpect(status().isOk())
                 .andReturn();
         assertEquals(ExpectedOutput, result.getResponse().getContentAsString());
